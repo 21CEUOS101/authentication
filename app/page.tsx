79e7cab2e04b5314266fd1bb6770de9fa0ac6b27
21CEUOS101 <21ceuos101@ddu.ac.in>
2024-01-05 23:58:33 +0530

    just started

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <Button>
-      Click Me
-    </Button>
+    <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+      <div className="space-y-6">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+      </div>
+    </main>
   )
 }
